a75fe3e304867af3c0af350d96e951cf1ecd3fe9
Gilang Andhika Erlangga <gilangandhikaerlangga18@gmail.com>
2025-02-17 15:53:23 +0700

    josheet 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Nama: Gilang Andhika Erlangga, NIM: 2341760129';
+    }
+}
